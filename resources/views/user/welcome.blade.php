7f169a9b8e7ec85a36eda6fbd4bf7207d7a98e75
andipurnm <andipurnono@gmail.com>
2021-06-08 00:07:37 -0700

    project

--- resources/views/user/welcome.blade.php
+++ resources/views/user/welcome.blade.php
@@ -1,6 +1,9 @@
 @extends('user.app')
 @section('content')
 
+
+
+<body>
  <!-- Latest Product -->
  <div class="banner">
 			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
@@ -28,23 +31,6 @@
 
 						</div>
 					</div>
-					<div class="carousel-item item3">
-						<div class="carousel-caption text-center">
-							<h3>Men’s eyewear
-								<span>Cool summer sale 50% off</span>
-							</h3>
-							<a href="shop.html" class="btn btn-sm animated-button gibson-three mt-4">Shop Now</a>
-
-						</div>
-					</div>
-					<div class="carousel-item item4">
-						<div class="carousel-caption text-center">
-							<h3>Women’s eyewear
-								<span>Want to Look Your Best?</span>
-							</h3>
-							<a href="shop.html" class="btn btn-sm animated-button gibson-three mt-4">Shop Now</a>
-						</div>
-					</div>
 				</div>
 				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
 					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -56,53 +42,10 @@
 				</a>
 			</div>
 			<!--//banner -->
-		</div>
-                    <div class="col-12">
-                        <div class="row">
-                            <div class="col-12 py-3">
-                                <div class="row">
-                                    <div class="col-12 text-center text-uppercase">
-                                        <h3>Latest Products</h3>
-                                    </div>
-                                </div>
-								
-                                <div class="row">
+</div>
+</body>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
-                                    <!-- Product -->
-									@foreach($produks as $produk)
-                                    <div class="col-lg-3 col-sm-6 my-3">
-                                        <div class="col-12 bg-white text-center h-100 product-item">
-										<span class="product-new-top">New</span>
-                                            <div class="row h-100">
-                                                <div class="col-12 p-0 mb-3">
-                                                    <a href="{{ route('user.produk.detail',['id' =>  $produk->id]) }}">
-                                                        <img src="{{ url('uploads')}}/{{ $produk->image }}" class="img-fluid">
-                                                    </a>
-                                                </div>
-                                                <div class="col-12 mb-3">
-                                                    <a href="{{ route('user.produk.detail',['id' =>  $produk->id]) }}" class="product-name">{{ $produk->name }}</a>
-                                                </div>
-                                                <div class="col-12 mb-3">
-												<div class="grid-price mt-2">
-													<span class="money ">Rp. {{ $produk->price }},00</span>
-												</div>
-                                                </div>
-                                                <div class="col-12 mb-3 align-self-end">
-													<a href="{{ route('user.produk.detail',['id' =>  $produk->id]) }}">
-                                                    <button class="btn btn-outline-dark" type="button"><i class="fas fa-cart-plus me-2"></i>Detail</button>
-                                                </div>
-												
-                                            </div>
-                                        </div>
-                                    </div>
-									@endforeach
-                                    <!-- Product -->
-                                </div>
-                            </div>
-                        </div>
-                    </div>  
-                </div>
-                   
-                    
-
-    @endsection
+@endsection
